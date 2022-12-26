114e711e742c0a3bec26dc0547d9819052a0035c
Rafael Stauffer Perez <rafaelsperez@vivaldi.net>
2022-12-26 22:44:52 +0000

    setup class base

---
+++ boto_connect.py
@@ -0,0 +1,33 @@
+import boto3
+
+class Connection:
+    def __init__(self, self.key):
+        self.key = ''
+
+    def get(key: str) -> BytesIO:
+        pass
+
+    def put(key: str, value: BytesIO):
+        pass
+
+    def pop(key: str) -> BytesIO:
+        pass
+
+    def __getitem__(key: str) -> BytesIO:
+        pass
+
+    def __setitem__(key: str, value: BytesIO):
+        pass
+
+    def __delitem__(key: str):
+        pass
+
+    def __contains__(key: str) -> bool:
+        pass
+
+    def keys(prefix: str ='') -> str:
+        pass
+
+    def items(prefix: str='') ->str:
+        pass
+
