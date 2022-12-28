12308ff5efa10108370955d4fe464fda347ec50c
Rafael Stauffer Perez <rafaelsperez@vivaldi.net>
2022-12-28 13:40:55 +0000

    rebase functions and test

--- boto_connect.py
+++ boto_connect.py
@@ -1,55 +1,54 @@
 import boto3
 import io
 
+
 class S3(object):
     """_summary_
 
     Args:
         object (_type_): _description_
     """
-    bucket_name = "test-for-boto"
 
-    def __init__(self, *args, **kwargs):
-        region = self.kwargs.get('region_name', 'us-east-1')
-        self.bucket_name = self.kwargs.get('bucket_name', self.bucket_name)
-        self.conn = client('s3', region_name=region)
-
+    def __init__(self, **kwargs):
+        self.region      = kwargs.get('region_name', 'eu-west-1')
+        self.bucket_name = kwargs.get('bucket_name', 'test-for-boto')
+        self.conn        = boto3.client('s3', region_name=self.region)
 
     def get(self, key: str):
         try:
-            response = client.get_object(
+            response = self.conn.get_object(
                 Bucket=self.bucket_name,
                 Key=key
             )
             return response
-        except Exception(e):
+        except Exception as e:
             return e
          
 
-    def put(self, key: str, value: BytesIO):
+    def put(self, key: str, value: str):
         try:
             response = client.put_object(
                 Bucket=self.bucket_name,
                 Key=key
             )
-        return "OK"
-        except Exception(e):
+            return "OK"
+        except Exception as e:
             return e
 
-    def pop(self, key: str) -> BytesIO:
+    def pop(self, key: str) -> str:
         try:
             get(key)
             response = client.delete_object(
                 Bucket=self.bucket_name,
                 Key=key
             )
-        except Exception(e):
+        except Exception as e:
             return e
 
-    def __getitem__(self, key: str) -> BytesIO:
+    def __getitem__(self, key: str) -> str:
         get(key)
 
-    def __setitem__(self, key: str, value: BytesIO):
+    def __setitem__(self, key: str, value: str):
         put(key)
 
     def __delitem__(key: str):
@@ -64,3 +63,6 @@ class S3(object):
     def items(prefix: str='') -> tuple:
         pass
 
+if __name__ == '__main__':
+    test_dict = {'bucket_name':'test-FOR-boto','region_name':'EU-west-1'}
+    test = S3(**test_dict)
