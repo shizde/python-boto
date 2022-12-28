bbb9d483d5b660390d2c11f37ef41eb7dc4f75f8
Rafael Stauffer Perez <rafaelsperez@vivaldi.net>
2022-12-28 20:10:36 +0000

    finishing base functions

--- boto_connect.py
+++ boto_connect.py
@@ -54,30 +54,27 @@ class S3(object):
         self.pop(key)
         return None
 
-    def list_bucket_objects(self, key: str='') -> list:
-        bucket_objects = []
-        for item in self.conn.list_objects(Bucket=self.bucket_name)['Contents']:
-            if key:
-                if key in item:
-                    bucket_objects.append(item['Key'])
-            else:
-                bucket_objects.append(item['Key'])
-        return bucket_objects
-
     def __contains__(self, key: str) -> bool:
-        if key in self.list_bucket_objects():
-            return True
-        else: 
-            return False
+        check = False
+        for item in self.conn.list_objects(Bucket=self.bucket_name)['Contents']:
+            if key == item['Key']:
+                check = True    
+        
+        return check
 
     def keys(self, prefix: str ='') -> str:
-        if prefix:
-            list_bucket_objects(prefix)
-        else:
-            list_bucket_objects()
+        for item in self.conn.list_objects(Bucket=self.bucket_name)['Contents']:
+            if prefix:
+                if prefix in item['Key']:
+                    yield item['Key']
+            else:
+                yield item['Key']
 
-    def items(prefix: str='') -> tuple:
-        pass
+    def items(self, prefix: str='') -> tuple:
+        for item in self.conn.list_objects(Bucket=self.bucket_name)['Contents']:
+            for k in item:
+                if k == 'Key':
+                    yield (k, item[k])
 
 if __name__ == '__main__':
     test_dict = {'bucket_name':'test-for-boto','region_name':'eu-west-1'}
